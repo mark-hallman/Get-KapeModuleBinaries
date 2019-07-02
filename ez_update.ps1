b7fd18cf8e400c8432c6c616d161d39d2bd21c5e
mark-hallman <mark.hallman@gmail.com>
2019-07-01 21:05:15 -0500

    Updated messages

--- ez_update.ps1
+++ ez_update.ps1
@@ -3,19 +3,22 @@
     This is a wrapprer script that updates Zimmermantools, KAPE, KAPE Targets, Modules, and Module binaries
 #>
 
-$zimmerman_tools_loc = "C:\Forensic Program Files\ZimmermanTools"
-$kape_loc            = "C:\Forensic Program Files\KAPE"
-$command = ".\Get-KapeModuleBinaries-DEV.ps1 -ModulePath $kape_loc\Modules -UseBinaryList -BinaryListPath .\KAPE-Default-Binaries.txt -dest 'C:\Forensic Program Files\ZimmermanTools\bin'"
+$ZimmermanToolsLocation = "C:\Tools\ZimmermanTools"
+$KapeLocation           = "C:\Tools\KAPE"
+$BinaryListPath         = "KAPE-Default-Binaries.txt"
+$BinUpdateCommand = ".\Get-KapeModuleBinaries-DEV.ps1 -ModulePath $KapeLocation\Modules -UseBinaryList -BinaryListPath $KapeLocation\$BinaryListPath -dest '$KapeLocation\Modules\bin'"
 
-Write-Host "`nUpdating Zimmerman Tools in Folder, $zimmerman_tools_loc`n" -ForegroundColor Green
-Set-Location -Path $zimmerman_tools_loc               # cd to Zimmermantools folder
+Write-Host "`nUpdating Zimmerman Tools in Folder $ZimmermanToolsLocation`n" -ForegroundColor Green
+Set-Location -Path $ZimmermanToolsLocation            # cd to Zimmermantools folder
 Invoke-Expression -Command .\Get-ZimmermanTools.ps1   # run EZ's tool update script
-Write-Host "`nUpdating KAPE in Folder, $kape_loc`n" -ForegroundColor Green
-Set-Location -Path $kape_loc                          # cd to KAPE folder
+
+Write-Host "`nUpdating KAPE in Folder $KapeLocation`n" -ForegroundColor Green
+Set-Location -Path $KapeLocation                      # cd to KAPE folder
 Invoke-Expression -Command .\Get-KAPEUpdate.ps1       # run EZ's KAPE update script
-Write-Host "`nUpdating KAPE Targets and Modules in Folder`n, $kape_loc`n" -ForegroundColor Green
+
+Write-Host "`nUpdating KAPE Targets and Modules in Folder $KapeLocation`n" -ForegroundColor Green
 Invoke-Command -ScriptBlock {kape --sync}             # run KAPE --sync to get new Targets & Modules
-Set-Location -Path $kape_loc                          # cd to KAPE folder (should still be there)
-Write-Host "`nUpdating KAPE Modules Binaries in Folder`n, $kape_loc`n" -ForegroundColor Green
-Invoke-Expression -Command $command                   # run command to update KAPE binaries.  See script for more details
-Write-Host "`nUpdating KAPE Modules Binaries in Folder`n, $kape_loc`n" -ForegroundColor Green
+
+Write-Host "`nUpdating KAPE Modules Binaries in Folder $KapeLocation`n" -ForegroundColor Green
+Invoke-Expression -Command $BinUpdateCommand          # run command to update KAPE binaries.  See script for more details
+Write-Host "`EZ Update Complete ...`n" -ForegroundColor Green
